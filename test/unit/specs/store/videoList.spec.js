a24cbfe26d9f7f0885163a1138a79c1568aa78d0
WonderToys <wondertoysvg@gmail.com>
2017-05-21 19:51:51 -0400

    Name tests properly.

--- test/unit/specs/store/videoList.spec.js
+++ test/unit/specs/store/videoList.spec.js
@@ -17,63 +17,65 @@ const mockState = {
 //  Mutations
 // -----
 
-describe('mutations', () => {
-  // RECEIVE_VIDEO_LIST
-  it(types.RECEIVE_VIDEO_LIST, () => {
-    const videos = [{
-      id: '-1',
-      snippet: {
-        title: 'Test'
-      }
-    }];
-    videoList.mutations[types.RECEIVE_VIDEO_LIST](mockState, { videos });
+describe('videoList Store', () => {
+  describe('mutations', () => {
+    // RECEIVE_VIDEO_LIST
+    it(types.RECEIVE_VIDEO_LIST, () => {
+      const videos = [{
+        id: '-1',
+        snippet: {
+          title: 'Test'
+        }
+      }];
+      videoList.mutations[types.RECEIVE_VIDEO_LIST](mockState, { videos });
 
-    const expected = [{ id: '-1', title: 'Test' }];
-    expect(mockState.videos).to.deep.equal(expected);
-  }); //- RECEIVE_VIDEO_LIST
+      const expected = [{ id: '-1', title: 'Test' }];
+      expect(mockState.videos).to.deep.equal(expected);
+    }); //- RECEIVE_VIDEO_LIST
 
-  // RECEIVE_VIDEO
-  it(types.RECEIVE_VIDEO, () => {
-    const video = {
-      id: '-1',
-      snippet: {
-        title: 'Test'
-      }
-    };
-    videoList.mutations[types.RECEIVE_VIDEO](mockState, { video });
+    // RECEIVE_VIDEO
+    it(types.RECEIVE_VIDEO, () => {
+      const video = {
+        id: '-1',
+        snippet: {
+          title: 'Test'
+        }
+      };
+      videoList.mutations[types.RECEIVE_VIDEO](mockState, { video });
 
-    expect(mockState.video).to.deep.equal(video);
-  }); //- RECEIVE_VIDEO
+      expect(mockState.video).to.deep.equal(video);
+    }); //- RECEIVE_VIDEO
 
-  // RECEIVE_VIDEO_COMMENTS
-  it(types.RECEIVE_VIDEO_COMMENTS, () => {
-    let comments = [{
-      id: '-1',
-      snippet: {
-        topLevelComment: {
-          snippet: {
-            title: 'Test'
+    // RECEIVE_VIDEO_COMMENTS
+    it(types.RECEIVE_VIDEO_COMMENTS, () => {
+      let comments = [{
+        id: '-1',
+        snippet: {
+          topLevelComment: {
+            snippet: {
+              title: 'Test'
+            }
           }
         }
-      }
-    }];
-    videoList.mutations[types.RECEIVE_VIDEO_COMMENTS](mockState, { comments });
+      }];
+      videoList.mutations[types.RECEIVE_VIDEO_COMMENTS](mockState, { comments });
 
-    let expected = [{ id: '-1', title: 'Test' }];
-    expect(mockState.commentThreads).to.deep.equal(expected);
+      let expected = [{ id: '-1', title: 'Test' }];
+      expect(mockState.commentThreads).to.deep.equal(expected);
 
-    comments = [];
-    videoList.mutations[types.RECEIVE_VIDEO_COMMENTS](mockState, { comments });
+      comments = [];
+      videoList.mutations[types.RECEIVE_VIDEO_COMMENTS](mockState, { comments });
 
-    expect(mockState.commentPageToken).to.equal(null);
-    expect(mockState.commentThreads).to.deep.equal(comments);
-  }); //- RECEIVE_VIDEO_COMMENTS
+      expect(mockState.commentPageToken).to.equal(null);
+      expect(mockState.commentThreads).to.deep.equal(comments);
+    }); //- RECEIVE_VIDEO_COMMENTS
 
-  // RECEIVE_COMMENT_PAGE_TOKEN
-  it(types.RECEIVE_COMMENT_PAGE_TOKEN, () => {
-    const token = '123';
-    videoList.mutations[types.RECEIVE_COMMENT_PAGE_TOKEN](mockState, { token });
+    // RECEIVE_COMMENT_PAGE_TOKEN
+    it(types.RECEIVE_COMMENT_PAGE_TOKEN, () => {
+      const token = '123';
+      videoList.mutations[types.RECEIVE_COMMENT_PAGE_TOKEN](mockState, { token });
 
-    expect(mockState.commentPageToken).to.deep.equal(token);
-  }); //- RECEIVE_COMMENT_PAGE_TOKEN
+      expect(mockState.commentPageToken).to.deep.equal(token);
+    }); //- RECEIVE_COMMENT_PAGE_TOKEN
+  });
 });
